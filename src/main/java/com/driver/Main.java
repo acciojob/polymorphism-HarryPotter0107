cfbd0066ca29b97d6b420acc43f541e7d66a5b5b
HarryPotter0107 <bhanuupadhyay87@gamil.com>
2023-09-29 03:56:48 +0530

    new change

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,22 @@
 package com.driver;
 
 public class Main {
-
+    public static void main(String[] args) {
+        Product p = new Product();
+        int ans = p.product(10,20);
+        int ans1 = p.product(10,20,30);
+        double ans2 = p.product(10,40);
+        System.out.println(ans+ " "+ ans1+" " + ans2);
+    }
+    static  class Product{
+        int product(int x, int y){
+            return x * y;
+        }
+        int product(int x, int y,int z){
+            return x * y * z;
+        }
+        double product(double x, double y){
+            return x * y;
+        }
+    }
 }
